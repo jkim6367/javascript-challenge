183d787858eafcb58d89df094e574accaa741f81
jkim6367 <jkim6367@gmail.com>
2020-02-26 21:31:36 -0500

    commit all

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,43 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+function ufoTable(sighting) {
+
+        var tbody = d3.select("tbody")
+
+    tbody.html("")
+
+    sighting.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(function([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
+
+ufoTable(tableData);
+
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value")
+
+    var parseDate = d3.timeParse("%m/%d/%Y");
+    var inputDate = parseDate(inputValue);
+
+    if (inputDate !==null) {
+    
+        console.log(inputDate);
+
+        var filteredData = tableData.filter(sighting => (parseDate(sighting.datetime).getTime() === inputDate.getTime()));
+
+        console.log(filteredData);
+        ufoTable(filteredData);
+
+    } else {
+        
+        ufoTable([]);
+    }
+});
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,27 @@
 // from data.js
-var tableData = data;
+var tableData = data.map(function (info) {
+    return {
+        "datetime": info.datetime,
+        "city": info.city,
+        "state": info.state.toUpperCase(),
+        "country":info.country.toUpperCase(),
+        "shape":info.shape,
+        "durationMinutes":info.durationMinutes,
+        "comments":info.comments
+    }
+});
+console.log(tableData)
 
-// YOUR CODE HERE!
+var countryUniqueList = [""];
+var countryList = tableData.map(function(info) {
+    return info.country
+});
+
+countryUnique = countryList.filter(UniqueFilter);
+console.log(countryUnique);
+
+countryUniqueList = countryUniqueList.concat(countryUnique.sort());
+console.log(countryUniqueList)
+
+var stateUniqueList = [{"country":"","state":""}];
+
